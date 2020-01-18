ee02a3395823f1c9c77a3c6e8e02512bed6750de
bobgit <goodfrar@126.com>
2020-01-18 13:31:41 +0800

    完善成为小型的项目，同时测试递交

--- src/main/java/github/com/bobgit/study/pinyin/PinyinApplication.java
+++ src/main/java/github/com/bobgit/study/pinyin/PinyinApplication.java
@@ -3,10 +3,12 @@ package github.com.bobgit.study.pinyin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("github.com.bobgit.study.pinyin.dao")  //自动扫描注册为 Spring Bean
 
+@EnableTransactionManagement //事务管理
 //@EnableJms //允许消息队列
 public class PinyinApplication {
 
--- src/main/java/github/com/bobgit/study/pinyin/controller/WordController.java
+++ src/main/java/github/com/bobgit/study/pinyin/controller/WordController.java
@@ -1,23 +1,35 @@
 package github.com.bobgit.study.pinyin.controller;
 
+import com.github.liuzhuoming23.pinyin4j.builder.HanyuPinyinOutputFormatBuilder;
+import com.github.liuzhuoming23.pinyin4j.helper.HanyuPinyinHelper;
+import com.google.common.collect.Lists;
 import github.com.bobgit.study.pinyin.common.CommonResponse;
+import github.com.bobgit.study.pinyin.common.ListResponse;
+import github.com.bobgit.study.pinyin.model.Word;
+import github.com.bobgit.study.pinyin.requestParam.WordRequest;
+import github.com.bobgit.study.pinyin.service.WordService;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ApiModel(value = "用户请求接口")
 @RestController
@@ -25,6 +37,146 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/word")
 @Slf4j
 public class WordController {
+    @Autowired
+    private HanyuPinyinOutputFormatBuilder hanyuPinyinOutputFormatBuilder;
+    @Autowired
+    private WordService wordService;
+
+    @ApiOperation(value = "retainAll")
+    @RequestMapping(value = "/retainAll", method = {RequestMethod.POST})
+    public ListResponse<Word> retainAll(HttpServletRequest request, HttpServletResponse response, @ApiParam(value = "上门就诊请求接口") @RequestBody WordRequest params) {
+//  固定长度  从源码的注释中可以看到，调用asList方法返回的是一个固定大小的集合，所以再往里添加元素必然会报错
+//        List<String> list1 =  Arrays.asList("无","高血压","糖尿病","肾病","脑卒中","心脏类疾病","免疫系统疾病","过敏","其他");//  Arrays.asList(pinyin.toCharArray());
+//        List<String> list2 = Arrays.asList("无1","高血压","糖尿病3","肾病","脑卒中5","心脏类疾病","免疫系统疾病7","过敏","其他9");
+
+        List<String> list1 = Lists.newArrayList(Arrays.asList("无","高血压","糖尿病","肾病","脑卒中","心脏类疾病","免疫系统疾病","过敏","其他")) ;//  Arrays.asList(pinyin.toCharArray());
+        List<String> list2 = Lists.newArrayList(Arrays.asList("无1","高血压","糖尿病3","肾病","脑卒中5","心脏类疾病","免疫系统疾病7","过敏","其他9"));
+
+
+        //交集
+        List<String> collect1 = list1.stream().filter(num -> list2.contains(num)).collect(Collectors.toList());
+        log.info("拼音的交集是:{}",collect1);
+        collect1.stream().forEach(System.out::println);
+
+        //差集 list1-list2
+        List<String> collect2 = list1.stream().filter(num -> !list2.contains(num)).collect(Collectors.toList());
+        log.info("拼音的差集:{}",collect2);
+//        collect2.stream().forEach(log::info);
+
+
+        //并集  不去重
+        log.info("list1:{}  list1=!=null:{}",list1,list1==null);
+        list1.addAll(list2);
+        log.info("并集  不去重:{}",list1);
+        //并集  去重
+        List<String> collect4 = list1.stream().distinct().collect(Collectors.toList());
+        log.info("并集  去重:{}",collect4);
+
+
+        ListResponse<Word> res = new ListResponse<Word>();
+params.setStart(15);
+params.setSize(200);
+log.info("params:{}",params);
+
+        String shengmu = "b p m f d t n l g k h j q x zh ch sh r z c s y w";
+        String[] shengmuArray = shengmu.split(" ");
+        List<String> shengmuList = Lists.newArrayList(shengmuArray);
+        log.info("声母 数量:{} 结果:{}",shengmuList.size(),shengmuList);
+        String aeiou = "āáăàaēéĕèeīíĭìiōóŏòoūúŭùuǖǘǚǜü";
+        String sone1 = "āēīōūǖ";
+        String sone2 = "áéíóúǘ";
+        String sone3 = "ăĕĭŏŭǚ";
+        String sone4 = "àèìòùǜ";
+        String vowela = "āáăàa";
+        String vowele = "ēéĕèe";
+        String voweli = "īíĭìi";
+        String vowelo = "ōóŏòo";
+        String vowelu = "ūúŭùuǖǘǚǜü";
+        List<String> sone1List = stringToListString(sone1);
+        List<String> sone2List = stringToListString(sone2);
+        List<String> sone3List = stringToListString(sone3);
+        List<String> sone4List = stringToListString(sone4);
+        List<Word> list = wordService.listWord(params);
+        log.info("list:{}",list);
+        res.setData(list);
+        list.forEach(t->{
+            String pinyin = t.getPinyin();
+            log.info("pinyin:{}",pinyin);
+            List<String> pinyinList = stringToListString(pinyin);
+
+            List<String> collectsoneShengMuList = pinyinList.stream().filter(num -> shengmuList.contains(num)).collect(Collectors.toList());
+            if(collectsoneShengMuList.size()>0){
+                t.setSyllable(collectsoneShengMuList.get(0));
+            }
+            List<String> collectsone1List = pinyinList.stream().filter(num -> sone1List.contains(num)).collect(Collectors.toList());
+            if(collectsone1List.size()>0){
+                log.info("-----------------：{}",collectsone1List);
+                t.setTone("1");
+            }
+            else{
+                List<String> collectsone2List = pinyinList.stream().filter(num -> sone2List.contains(num)).collect(Collectors.toList());
+                if(collectsone2List.size()>0){
+                    log.info("=============：{}",collectsone2List);
+                    t.setTone("2");
+                }
+                else{
+                    List<String> collectsone3List = pinyinList.stream().filter(num -> sone3List.contains(num)).collect(Collectors.toList());
+                    if(collectsone3List.size()>0){
+                        log.info("##################：{}",collectsone3List);
+                        t.setTone("3");
+                    }
+                    else{
+                        List<String> collectsone4List = pinyinList.stream().filter(num -> sone4List.contains(num)).collect(Collectors.toList());
+                        if(collectsone4List.size()>0){
+                            log.info("4444444444444444444：{}",collectsone4List);
+                            t.setTone("4");
+                        }
+                        else{
+                            log.info("轻声轻声轻声轻声轻声轻声轻声轻声轻声轻声");
+                            t.setTone("0");
+                        }
+                    }
+                }
+            }
+
+        });
+        return res;
+    }
+
+    public List<String> stringToListString(String str){
+        List<String> charListString = Lists.newArrayList();
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            charListString.add(""+charArray[i]);
+        }
+        log.info("数组情况charListString:{}",charListString);
+        return charListString;
+    }
+
+
+
+    @ApiOperation(value = "pinyin")
+    @RequestMapping(value = "/pinyin", method = {RequestMethod.GET})
+    public CommonResponse<String> pinyin(HttpServletRequest request, HttpServletResponse response,@RequestParam(value = "file", required = false, defaultValue = "社会主义核心价值观") String file) {
+        CommonResponse<String> res = new CommonResponse<String>();
+        HanyuPinyinOutputFormat hanyuPinyinOutputFormat = hanyuPinyinOutputFormatBuilder
+                .toneType(HanyuPinyinToneType.WITH_TONE_MARK)
+                .vCharType(HanyuPinyinVCharType.WITH_U_UNICODE)
+                .build();
+        String pinyin = null;
+        try {
+            pinyin = HanyuPinyinHelper.toHanYuPinyinString(file, hanyuPinyinOutputFormat, " ", true);
+        } catch (BadHanyuPinyinOutputFormatCombination badHanyuPinyinOutputFormatCombination) {
+            badHanyuPinyinOutputFormatCombination.printStackTrace();
+        }
+        String aeiou = "āáăàaēéĕèeīíĭìiōóŏòoūúŭùuǖǘǚǜü";
+        String sone1 = "āēīōūǖ";
+        String sone2 = "áéíóúǘ";
+        String sone3 = "ăĕĭŏŭǚ";
+        String sone4 = "àèìòùǜ";
+        res.setData(pinyin);
+        return res;
+    }
 
 
     @ApiOperation(value = "test")
