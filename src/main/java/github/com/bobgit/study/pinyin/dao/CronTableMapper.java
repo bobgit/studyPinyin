dc1331327db85b865ab3e77884f44a64ced5fdb9
bob <goodfrar@126.com>
2020-03-08 21:49:11 +0800

    pom 依赖方面的订正

--- src/main/java/github/com/bobgit/study/pinyin/dao/CronTableMapper.java
+++ src/main/java/github/com/bobgit/study/pinyin/dao/CronTableMapper.java
@@ -1,12 +1,7 @@
 package github.com.bobgit.study.pinyin.dao;
 
 import github.com.bobgit.study.pinyin.model.CronTable;
-import org.springframework.stereotype.Repository;
 
-/**
- * 字词信息
- */
-@Repository
 public interface CronTableMapper {
     int deleteByPrimaryKey(Integer id);
 
